6a4041b48de9200928e4cf658f42c2be005a2054
Bitan Biswas <76226078+bitanb1999@users.noreply.github.com>
2024-09-22 20:23:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from llama_index.core import VectorStoreIndex, ServiceContext, Document
-from llama_index import Settings
+from llama_index.settings.Settings import Settings
 from llama_index.llms.openai import OpenAI
 import openai
 from llama_index.core import SimpleDirectoryReader
