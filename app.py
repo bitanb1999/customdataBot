a127cc21a8f65527018e05ad5ba6026f3a524275
Bitan Biswas <76226078+bitanb1999@users.noreply.github.com>
2024-09-22 20:29:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        Settings.llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the ARIBA platform and  and your job is to answer technical questions. Assume that all questions are related to ARIBA and SAP. Keep your answers technical and based on facts – do not hallucinate features.")
-        index = VectorStoreIndex.from_documents(docs, llm=llm)
+        settings = Settings(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the ARIBA platform and your job is to answer technical questions. Assume that all questions are related to ARIBA and SAP. Keep your answers technical and based on facts – do not hallucinate features."))
+        index = VectorStoreIndex.from_documents(docs, settings=settings)
         return index
 
 index = load_data()
